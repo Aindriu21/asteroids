147a222cde7e80937cee403fb46ec99c026d9167
Aindriu21 <valandil.saralonde94@gmail.com>
2024-12-02 23:31:05 -0500

    added a Shot class and a .split() method for the asteroids

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import sys
 import pygame
 from constants import *
 from player import Player 
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -12,12 +14,14 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
 
-    Player.containers = (updatable, drawable)
+    Player.containers = ( updatable, drawable)
 
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
@@ -40,6 +44,21 @@ def main():
 
 
         pygame.display.flip()
+
+        for asteroid in asteroids:
+            for bullet in shots:
+                if bullet.collides(asteroid):
+                    #pygame.sprite.Sprite.kill(asteroid)
+                    #pygame.sprite.Sprite.kill(bullet)
+                    bullet.kill()
+                    asteroid.split()
+                    
+
+            if asteroid.collides(player):
+                print("Game over!")
+                sys.exit()
+
+            
         
         # limiting the framework to 60 FPS
         dt = clock.tick(60)
